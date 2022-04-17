22fc76925c12385a8977f7dc0c6e38e019631b75
swiftJules <101522134+swiftJules@users.noreply.github.com>
2022-04-17 08:18:29 -0400

    update project file

---
+++ GuessTheFlag/BlueTitle.swift
@@ -0,0 +1,20 @@
+//
+//  BlueTitle.swift
+//  GuessTheFlag
+//
+//  Created by RBS70 on 4/17/22.
+//
+
+import SwiftUI
+
+struct BlueTitle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlueTitle_Previews: PreviewProvider {
+    static var previews: some View {
+        BlueTitle()
+    }
+}
---
+++ GuessTheFlag/FlagImage.swift
@@ -0,0 +1,20 @@
+//
+//  FlagImage.swift
+//  GuessTheFlag
+//
+//  Created by RBS70 on 4/17/22.
+//
+
+import SwiftUI
+
+struct FlagImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FlagImage_Previews: PreviewProvider {
+    static var previews: some View {
+        FlagImage()
+    }
+}
